eeeb575dbc86bdfbacaaf1086e7043576afe9a57
Justin Shi <justin.matthew.shi@gmail.com>
2017-12-29 23:38:46 -0500

    added functionality for selecting line to choose side pixelation should be performed upon, slider for pixel side, side flipping

--- js/index.js
+++ js/index.js
@@ -1,17 +1,155 @@
 let hasImage = false;
 let fileInput = null;
 let imageCanvas = null;
+let pixelSizeSlider = null;
+let origImageData = null;
+let currImageData = null;
+let pixelateAbove = true;
+const points = [null, null];
 const reader = new FileReader();
 const img = new Image();
 const maxSize = 500;
 
+const switchPixelateSide = () => {
+	pixelateAbove = !pixelateAbove;
+	pixelate();
+	if (points[0]) {
+		drawPoint('#FF0000', points[0]);
+	}
+	if (points[1]) {
+		drawPoint('#0000FF', points[1]);
+
+		// formula for a line: y - y_0 = m * (x - x_0)
+		drawLine();
+	}
+}
+
+const drawLine = () => {
+	const context = imageCanvas.getContext('2d');
+	context.beginPath();
+	context.strokeStyle = '#00FF00';
+
+	// formula for a line: y - y_0 = m * (x - x_0)
+	const slope = points[1].x - points[0].x != 0 ? 1.0 * (points[1].y - points[0].y) / (points[1].x - points[0].x) : null;
+	const newPoints = [];
+	if (slope == null) {
+		newPoints.push({
+			x: points[0].x,
+			y: 0
+		});
+		newPoints.push({
+			x: points[0].x,
+			y: imageCanvas.height
+		});
+	}
+	else if (slope == 0) {
+		newPoints.push({
+			x: 0,
+			y: points[0].y
+		});
+		newPoints.push({
+			x: imageCanvas.width,
+			y: points[0].y
+		});
+	}
+	else {
+		const leftY = slope * (0 - points[0].x) + points[0].y;
+		const rightY = slope * (imageCanvas.width - points[0].x) + points[0].y;
+		const topX = points[0].x + (0 - points[0].y) / slope;
+		const botX = points[0].x + (imageCanvas.height - points[0].y) / slope;
+		if (newPoints.length < 2 && leftY >= 0 && leftY <= imageCanvas.height) {
+			newPoints.push({
+				x: 0,
+				y: leftY
+			});
+		}
+		if (newPoints.length < 2 && rightY >= 0 && rightY <= imageCanvas.height) {
+			newPoints.push({
+				x: imageCanvas.width,
+				y: rightY
+			});
+		}
+		if (newPoints.length < 2 && topX >= 0 && topX <= imageCanvas.width) {
+			newPoints.push({
+				x: topX,
+				y: 0
+			});
+		}
+		if (newPoints.length < 2 && botX >= 0 && botX <= imageCanvas.width) {
+			newPoints.push({
+				x: botX,
+				y: imageCanvas.height
+			});
+		}
+	}
+	context.moveTo(newPoints[0].x, newPoints[0].y);
+	context.lineTo(newPoints[1].x, newPoints[1].y);
+	context.stroke();
+	context.closePath();
+}
+
+const drawPoint = (color, point) => {
+	const context = imageCanvas.getContext('2d');
+	context.beginPath();
+	context.strokeStyle = color;
+	context.arc(point.x, point.y, 5, 0, 2 * Math.PI);
+	context.stroke();
+	context.closePath();
+}
+
 const init = () => {
 	fileInput = document.getElementById('imageUpload');
 	imageCanvas = document.getElementById('imageCanvas');
+	imageCanvas.addEventListener('click', (event) => {
+		const rect = imageCanvas.getBoundingClientRect();
+		const mouseX = event.clientX - rect.left;
+		const mouseY = event.clientY - rect.top;
+		if (points[1] || points[0] == null) {
+			pixelate();
+			points[0] = {
+				x: mouseX,
+				y: mouseY
+			};
+			points[1] = null;
+			drawPoint('#FF0000', points[0]);
+		}
+		else if (points[0]) {
+			if (mouseX == points[0].x && mouseY == points[0].y) {
+				return;
+			}
+			points[1] = {
+				x: mouseX,
+				y: mouseY
+			};
+			pixelate();
+			drawPoint('#FF0000', points[0]);
+			drawPoint('#0000FF', points[1]);
+
+			// formula for a line: y - y_0 = m * (x - x_0)
+			drawLine();
+		}
+	})
+	pixelSizeSlider = document.getElementById('pixelSizeSlider');
+	pixelSize = parseInt(pixelSizeSlider.value);
+}
+
+const pixelSizeChange = () => {
+	pixelSize = parseInt(pixelSizeSlider.value);
+	pixelate();
+	const context = imageCanvas.getContext('2d');
+	if (points[0]) {
+		drawPoint('#FF0000', points[0]);
+	}
+	if (points[1]) {
+		drawPoint('#0000FF', points[1]);
+
+		// formula for a line: y - y_0 = m * (x - x_0)
+		drawLine();
+	}
 }
 
 const drawImage = () => {
-	if (fileInput.files && fileInput.files[0]) {
+	if (fileInput.files && fileInput.files[0] && fileInput.files[0].type.match('image.*')) {
 		reader.onload = (event) => {
 			img.onload = () => {
 				let newWidth = maxSize;
@@ -23,7 +161,10 @@ const drawImage = () => {
 				imageCanvas.width = newWidth;
 				imageCanvas.height = newHeight;
 				imageCanvas.getContext('2d').drawImage(img, 0, 0, newWidth, newHeight);
+				origImageData = imageCanvas.getContext('2d').getImageData(0, 0, imageCanvas.width, imageCanvas.height);
+				currImageData = origImageData;
 				hasImage = true;
+				pixelate();
 			};
 			img.src = event.target.result;
 		};
--- js/pixel.js
+++ js/pixel.js
@@ -1,16 +1,41 @@
-const pixelSize = 3; // need to make this a parameter the user can choose
+let pixelSize = 1;
+
+// determinant is flipped because Y-value grow downwards
+const belowLine = (x, y) => {
+	if (!(points[0] && points[1])) {
+		return false;
+	}
+	const slope = points[1].x - points[0].x != 0 ? 1.0 * (points[1].y - points[0].y) / (points[1].x - points[0].x) : null;
+	if (slope == null) { // handles x = x_0 case
+		return x < points[0].x;
+	}
+	const det = slope * x - y + (points[0].y - slope * points[0].x);
+	return det < 0;
+}
 
 const pixelate = () => {
 	if (hasImage) {
 		const context = imageCanvas.getContext('2d');
-		const origImageData = context.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
 		const newImageData = context.createImageData(origImageData);
 		for (let i = 0; i < origImageData.height; i += pixelSize) {
 			for (let j = 0; j < origImageData.width; j += pixelSize) {
 				const topLeftIndex = 4 * i * origImageData.width + 4 * j;
+				const pixelWidth = Math.min(pixelSize, origImageData.width - j);
+				const pixelHeight = Math.min(pixelSize, origImageData.height - i);
+				if (belowLine(j + pixelSize / 2, i + pixelSize / 2) == pixelateAbove) {
+					for (let k = 0; k < pixelHeight; k++) {
+						for (let l = 0; l < pixelWidth; l++) {
+							newImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l] = origImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l];
+							newImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 1] = origImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 1];
+							newImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 2] = origImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 2];
+							newImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 3] = origImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 3];
+						}
+					}
+					continue;
+				}
 				let average = [0, 0, 0, 0];
-				for (let k = 0; k < pixelSize; k++) {
-					for (let l = 0; l < pixelSize; l++) {
+				for (let k = 0; k < pixelHeight; k++) {
+					for (let l = 0; l < pixelWidth; l++) {
 						average[0] += origImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l];
 						average[1] += origImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 1];
 						average[2] += origImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 2];
@@ -18,10 +43,10 @@ const pixelate = () => {
 					}
 				}
 				for (let k = 0; k < 4; k++) {
-					average[k] /= (pixelSize * pixelSize);
+					average[k] /= (pixelWidth * pixelHeight);
 				}
-				for (let k = 0; k < pixelSize; k++) {
-					for (let l = 0; l < pixelSize; l++) {
+				for (let k = 0; k < pixelHeight; k++) {
+					for (let l = 0; l < pixelWidth; l++) {
 						newImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l] = average[0];
 						newImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 1] = average[1];
 						newImageData.data[topLeftIndex + 4 * k * origImageData.width + 4 * l + 2] = average[2];
@@ -29,8 +54,8 @@ const pixelate = () => {
 					}
 				}
 			}
-			// need to handle left-over row/column
 		}
+		currImageData = newImageData;
 		context.putImageData(newImageData, 0, 0);
 	}
 }
